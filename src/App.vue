e5685d1fa140c7859c7bb1d1eb6f08792f78ec25
Bruno <bruno.oliniski@gmail.com>
2021-05-10 08:47:02 -0300

    Axios API

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,19 @@
 </template>
 
 <script>
-
+import axios from 'axios'
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      pokemons: []
+    }
+  },
+  created: function() {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
+      this.pokemons = res.data.results;
+    })
+  }
 }
 </script>
 
