6074686f8f8cb8337d31b51d6be878ea94b3b949
Bruno <bruno.oliniski@gmail.com>
2021-05-10 09:59:07 -0300

    List and search

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,42 @@
 <template>
   <div id="app">
-    
+    <div class="column is-half is-offset-one-quarter">
+      <img src="./assets/logo.png" alt="">
+      <input type="text" placeholder="Search..." v-model="search" class="input">
+      <div v-for="(poke, index) in searchResult" :key="index">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index + 1"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Pokemon from './components/Pokemon.vue'
 export default {
   name: 'App',
   data () {
     return {
-      pokemons: []
+      pokemons: [],
+      search: ''
     }
   },
   created: function() {
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
       this.pokemons = res.data.results;
     })
+  },
+  components: {
+    Pokemon
+  },
+  computed: {
+    searchResult: function() {
+      if (this.search == '' || this.search == ' ') {
+        return this.pokemons;
+      } else {
+        return this.pokemons.filter(pokemon => pokemon.name.match(this.search.toLocaleLowerCase()));
+      }
+    }
   }
 }
 </script>
